da1db3817d0b0ea2756f5a1dfd4910542d437252
allanescossio <allanjohnson.pe@gmail.com>
2022-02-05 11:10:56 -0300

    Inicia alteração de estrutura

--- app/mailers/invoice_mailer.rb
+++
@@ -1,9 +0,0 @@
-class InvoiceMailer < ApplicationMailer
-  def created
-    @invoice = params[:invoice]
-
-    emails = @invoice.emails.split(',')
-
-    mail(to: emails, subject: "Invoice #{@invoice.id}")
-  end
-end
--- app/models/invoice.rb
+++
@@ -1,5 +0,0 @@
-class Invoice < ApplicationRecord
-  after_save do
-    InvoiceMailer.with(invoice: self).created.deliver_now
-  end
-end
